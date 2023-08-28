ce036fb08cabc541e7bd5777a739e725265014f5
DVS <divyesh220998@gmail.com>
2023-08-28 18:22:21 +0530

    updated

---
+++ client/build/precache-manifest.31cccbf5f995529cfb87c7c59bc40073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb36e265fe518e2a9a13",
+    "url": "/static/css/main.a4c07e64.chunk.css"
+  },
+  {
+    "revision": "cb36e265fe518e2a9a13",
+    "url": "/static/js/main.cb36e265.chunk.js"
+  },
+  {
+    "revision": "108ad61f8321a1cbb394",
+    "url": "/static/js/1.108ad61f.chunk.js"
+  },
+  {
+    "revision": "e4a96a4c9f3732a169e2",
+    "url": "/static/js/2.e4a96a4c.chunk.js"
+  },
+  {
+    "revision": "ef96a34a0ca49c5c05cc",
+    "url": "/static/js/runtime~main.ef96a34a.js"
+  },
+  {
+    "revision": "4812a215a7f7b7fa648906c03298fc0e",
+    "url": "/index.html"
+  }
+];
